2ef794358e207ecd07b4c6ff2860db0b26bb0956
shalini-kprcas <144670582+shalini-kprcas@users.noreply.github.com>
2023-09-23 09:18:15 +0530

    Add files via upload

---
+++ Challenge 2.1.py
@@ -0,0 +1,60 @@
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid deposit amount. Please enter a positive value.")
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
+        else:
+class BankAccount:
+    def __init__(self, account_number, account_holder_name, initial_balance=0):
+        self.__account_number = account_number
+        self.__account_holder_name = account_holder_name
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        if amount > 0:
+            self.__account_balance += amount
+            print(f"Deposited ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid deposit amount. Please enter a positive value.")
+
+    def withdraw(self, amount):
+        if 0 < amount <= self.__account_balance:
+            self.__account_balance -= amount
+            print(f"Withdrew ${amount}. New balance: ${self.__account_balance}")
+        else:
+            print("Invalid withdrawal amount or insufficient funds.")
+
+    def display_balance(self):
+        print(f"Account Holder: {self.__account_holder_name}")
+        print(f"Account Number: {self.__account_number}")
+        print(f"Account Balance: ${self.__account_balance}")
+
+
+# Create an instance of BankAccount
+my_account = BankAccount("1234567890", "John Doe", 1000)
+
+# Test deposit and withdrawal functionality
+my_account.display_balance()
+my_account.deposit(500)
+my_account.withdraw(200)
+my_account.display_balance()￼Not            print("Invalid withdrawal amount or insufficient funds.")
+
+    def display_balance(self):
+        print(f"Account Holder: {self.__account_holder_name}")
+    print(f"Account Number: {self.__account_number}")
+        print(f"Account Balance: ${self.__account_balance}")
+
+
+# Create an instance of BankAccount
